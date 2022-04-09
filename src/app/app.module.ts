dc225127d3fd3c56651848832f66cd345a4b6c01
Charmain <mogerecharmain@gmail.com>
2022-04-09 12:38:37 +0300

    Created a new quotes component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
