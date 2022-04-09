a515d0619b210eb54464467e7e9e79a4aab08404
Charmain <mogerecharmain@gmail.com>
2022-04-09 17:27:44 +0300

    Created constructor function.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,21 @@
 export class Quote {
+
   id: number;
   quotetitle: string;
   quotecontent: string;
   author: string;
   submittedquote: string;
-  creationdate : Date;
-  like: number;
-  dislike: number;
+
+
+  constructor(id:number, quotetitle:string, quotecontent:string,author:string, submittedquote:string){
+    this.id = id
+    this.quotetitle = quotetitle
+    this.quotecontent = quotecontent
+    this.author = author
+    this.submittedquote = submittedquote
+  }
+
+  // creationdate : Date;
+  // like: number;
+  // dislike: number;
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '../quote'
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -9,11 +9,11 @@ import { Quote } from '../quote'
 export class QuotesComponent implements OnInit {
 
   quotes:Quote[]= [
-    {id:1, quotetitle:'Learning',quotecontent:'Play is the highest form of research.',author: 'Albert Einsten',submittedquote:'Salma'},
-    {id:2, quotetitle:'Learning',quotecontent:'Tell me and I forget, teach me and I may remember, involve me and I learn.', author:'Benjamin Franklin', submittedquote:'Kahinga'},
+    new Quote(1, 'Learning','Play is the highest form of research.', 'Albert Einsten','Salma'),
+    {id:2, quotetitle:'Learning',quotecontent:'"Tell me and I forget, teach me and I may remember, involve me and I learn."', author:'Benjamin Franklin', submittedquote:'Kahinga'},
     {id:3, quotetitle:'Learning', quotecontent:'He who laughs most learns best.', author:'John Cleese', submittedquote:'Charmain'},
     {id:4, quotetitle:'Design', quotecontent:'When UX doesn’t consider ALL users, shouldn’t it be known as “SOME User Experience” or… SUX?', author:'Billy Gregory', submittedquote:'Charmain'},
-    {id:5, quotetitle:'Design', quotecontent:'Art has to move you and design does not, unless it is a good design for a bus', author:'David Hockney',submittedquote:'Anastacia'},
+    {id:5, quotetitle:'Design', quotecontent:'Art has to move you and design does not, unless its’s a good design for a bus', author:'David Hockney',submittedquote:'Anastacia'},
     {id:6, quotetitle:'Design', quotecontent:'The life of a designer is a life of a fight, fight against the ugliness and dullness', author:'Massimo Vignelli', submittedquote:'Lawrencio'},
   ];
 
