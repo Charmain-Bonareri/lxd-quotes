828456d427f6e5d8b03c59862f1a2f2e13b77612
Charmain <mogerecharmain@gmail.com>
2022-04-10 04:48:02 +0300

    Generated new quote-detail component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
