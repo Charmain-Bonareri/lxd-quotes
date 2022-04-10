f39f5dba871c2f48ce5dc69bbfa21a6ed0c6de9c
Charmain <mogerecharmain@gmail.com>
2022-04-10 17:56:22 +0300

    Defined the logic to count the number of days.

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date(); //get current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(todayWithNoTime - value)
+    const secondsInADay= 86400;   //60 seconds*60 minutes in an hour * 24 hours in a day
+
+    var dateDifferenceSeconds=dateDifference*0.001; //converts miliseconds to seconds
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
+
+    if (dateCounter >= 1 && todayWithNoTime > value){
+        return dateCounter;
+    }else{
+        return 0;
+    }
   }
 
 }
