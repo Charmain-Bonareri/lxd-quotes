7e53cb6281923ca3d435f4b599278f821317fbc0
Charmain <mogerecharmain@gmail.com>
2022-04-10 04:12:53 +0300

    Defined each quote using new keyword and specified the properties in the constructor function.

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes:Quote[]= [
-    new Quote(1, 'Learning','Play is the highest form of research.', 'Albert Einsten','Salma'),
-    {id:2, quotetitle:'Learning',quotecontent:'"Tell me and I forget, teach me and I may remember, involve me and I learn."', author:'Benjamin Franklin', submittedquote:'Kahinga'},
-    {id:3, quotetitle:'Learning', quotecontent:'He who laughs most learns best.', author:'John Cleese', submittedquote:'Charmain'},
-    {id:4, quotetitle:'Design', quotecontent:'When UX doesn’t consider ALL users, shouldn’t it be known as “SOME User Experience” or… SUX?', author:'Billy Gregory', submittedquote:'Charmain'},
-    {id:5, quotetitle:'Design', quotecontent:'Art has to move you and design does not, unless its’s a good design for a bus', author:'David Hockney',submittedquote:'Anastacia'},
-    {id:6, quotetitle:'Design', quotecontent:'The life of a designer is a life of a fight, fight against the ugliness and dullness', author:'Massimo Vignelli', submittedquote:'Lawrencio'},
+    new Quote(1, 'Learning','"Play is the highest form of research."', '- Albert Einsten','Salma'),
+    new Quote(2, 'Learning','"Tell me and I forget, teach me and I may remember, involve me and I learn."', '- Benjamin Franklin','Kahinga'),
+    new Quote(3, 'Learning', '"He who laughs most learns best."','- John Cleese','Charmain'),
+    new Quote(4, 'Design', '"When UX doesn’t consider ALL users, shouldn’t it be known as “SOME User Experience” or… SUX?"', '- Billy Gregory', 'Charmain'),
+    new Quote(5, 'Design', '"Art has to move you and design does not, unless its’s a good design for a bus."', '- David Hockney','Anastacia'),
+    new Quote(6, 'Design', '"The life of a designer is a life of a fight, fight against the ugliness and dullness."', '- Massimo Vignelli', 'Lawrencio'),
   ];
 
 
