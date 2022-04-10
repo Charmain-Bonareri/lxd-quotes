451dedb9732368a5574e11a089e997bf4e4f7afb
Charmain <mogerecharmain@gmail.com>
2022-04-10 17:31:23 +0300

    Styled the highlight directive with cyan as the background color on click trigger.

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {}
 
+  @HostListener("click") upVotes(){
+    this.highlight("highlight")
+  }
+  @HostListener("dblclick") onDoubleClicks() {
+    this.highlight("highlight")
+  }
+  private highlight(highlight: string){
+    this.elem.nativeElement.style.backgroundColor = 'cyan';
+  }
 }
